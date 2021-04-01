6aa4ecfaf36dc297949048f9fd85428faa81cbb9
RanaJazim <stackdeveloper87@gmail.com>
2021-04-01 02:29:03 -0700

    hash the password before save to database ...

--- db/user.js
+++ db/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -11,6 +12,24 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre("save", hashPassword);
+function hashPassword(next) {
+  var user = this;
 
+  if (!user.password) return next();
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+}
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
