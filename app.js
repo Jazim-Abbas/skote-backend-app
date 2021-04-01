f40320d79043e38914c136c12f915c1b4d1dbe80
RanaJazim <stackdeveloper87@gmail.com>
2021-03-31 23:43:04 -0700

    added exception handling middleware ...

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+require("express-async-errors");
+
 const authRoutes = require("./routes/auth");
+const exceptionHandling = require("./middlewares/exception_handling");
 
 const app = express();
 
 app.use("/auth", authRoutes);
+app.use(exceptionHandling);
 
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App is listening on the port ${PORT}`));
---
+++ middlewares/exception_handling.js
@@ -0,0 +1,19 @@
+const Exception = require("../utils/custom_exceptions");
+
+module.exports = function (err, req, res, next) {
+  let statusCode = 500;
+  let message = "Server error";
+  let name = "SERVER_ERROR";
+  let errors = [];
+
+  if (err instanceof Exception.HttpError) {
+    if (err instanceof Exception.ValidationError) {
+      errors = err.errors;
+    }
+    statusCode = err.statusCode;
+    message = err.message;
+    name = err.name;
+  }
+
+  res.status(statusCode).send({ message, name, errors });
+};
