0cfec188187264a9f5d5084195792249987b519a
jazim-abbas <jazimabbas87@gmail.com>
2021-04-05 17:20:28 +0500

    send mail with token ...

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
+const mailer = require("../utils/send_email");
 const authService = require("../services/auth");
 const validation = require("../utils/validations/auth");
+const generateToken = require("../utils/generate_token");
 const setTokenInUser = require("../utils/set_user_token");
 const schemaValidate = require("../utils/validations/validate");
-const mailer = require("../utils/send_email");
 
 async function register(req, res) {
   const fields = await schemaValidate(validation.registerSchema, req.body);
@@ -29,16 +30,24 @@ async function passwordForget(req, res) {
 }
 
 async function sendEmailForVerification(req, res) {
+  const token = await generateToken(
+    { data: "email-verify-" + Math.random() },
+    "1d"
+  );
+
   try {
     await mailer.sendMail({
       to: req.body.email,
       subject: "Please verify your email",
-      html: "Please verify ..",
+      html: `
+        <h1>Please verify your email addres.</h1>
+        <a href='${token}'>Verify<a/>
+      `,
     });
     res.send("Check your email ...");
   } catch (err) {
     console.log(err);
-    res.send("Error");
+    res.status(400).send({ message: "Some problems while sending email" });
   }
 }
 
