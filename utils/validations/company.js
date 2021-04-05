186f1b4c1dc4c643875a854d91af397a8ebdbecb
jazim-abbas <jazimabbas87@gmail.com>
2021-04-05 13:39:54 +0500

    added company schema validation for forms ..

---
+++ utils/validations/company.js
@@ -0,0 +1,12 @@
+const yup = require("yup");
+
+const companySchema = yup.object().shape({
+  logo: yup.string().notRequired(),
+  bus_name: yup.string().max(100).required(),
+  bus_email: yup.string().max(300).required(),
+  bus_phone: yup.string().max(13).required(),
+  bus_address: yup.string().max(500).required(),
+  website_link: yup.string().url().required(),
+});
+
+module.exports = { companySchema };
