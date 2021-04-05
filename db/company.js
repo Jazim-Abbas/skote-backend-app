de9e8b0816567d7f010da46f24cb3354d11b826d
jazim-abbas <jazimabbas87@gmail.com>
2021-04-05 12:42:36 +0500

    added company schema ...

---
+++ db/company.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const companySchema = new mongoose.Schema({
+  logo: String,
+  bus_name: String,
+  bus_email: String,
+  bus_phone: String,
+  bus_address: String,
+  website_link: String,
+});
+
+const Company = mongoose.model("Company", companySchema);
+module.exports = Company;
