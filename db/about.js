be693739185bf610766d152c3a9cb2b1a2c32c81
jazim-abbas <jazimabbas87@gmail.com>
2021-04-05 09:48:02 +0500

    added about schema ...

---
+++ db/about.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const aboutSchema = new mongoose.Schema({
+  name: String,
+  role: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const About = mongoose.model("About", aboutSchema);
+module.exports = About;
