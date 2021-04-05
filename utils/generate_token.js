e787b2ef70b815683140456e28b5f82786f76b73
jazim-abbas <jazimabbas87@gmail.com>
2021-04-05 16:34:41 +0500

    generate tokens with expire time limit ...

--- utils/generate_token.js
+++ utils/generate_token.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-async function generateToken(payload) {
-  return jwt.sign(payload, config.get("jwt.secret"));
+async function generateToken(payload, expires = "30d") {
+  return jwt.sign(payload, config.get("jwt.secret"), { expiresIn: expires });
 }
 
 module.exports = generateToken;
