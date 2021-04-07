7c8658dd27eef98bf4817e7505bad34b0e15d96a
RanaJazim <stackdeveloper87@gmail.com>
2021-04-07 05:32:24 -0700

    delete the introduction record from database  ..

--- controllers/business/introduction.js
+++ controllers/business/introduction.js
@@ -16,8 +16,14 @@ async function update(req, res) {
   res.send({ introduction: introRecord });
 }
 
+async function destroy(req, res) {
+  await introductionService.destroy(req.params.id);
+  res.send({ message: "Successfully deleted the record .." });
+}
+
 module.exports = {
   store,
   fetchSingle,
   update,
+  destroy,
 };
--- routes/business/introduction.js
+++ routes/business/introduction.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router
   .post("/", introductionController.store)
   .get("/", introductionController.fetchSingle)
-  .patch("/:id", introductionController.update);
+  .patch("/:id", introductionController.update)
+  .delete("/:id", introductionController.destroy);
 
 module.exports = router;
