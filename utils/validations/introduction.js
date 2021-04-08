663ff3894a436460f748159dd02d4a345ac643de
RanaJazim <stackdeveloper87@gmail.com>
2021-04-07 22:32:04 -0700

    added validation schema for introduction section ...

---
+++ utils/validations/introduction.js
@@ -0,0 +1,9 @@
+const yup = require("yup");
+
+const introSchema = yup.object().shape({
+  bus_short_desc: yup.string().min(5).max(500).required(),
+  company_do: yup.string().min(5).max(500).required(),
+  products: yup.array().of(yup.string().min(3)),
+});
+
+module.exports = { introSchema };
