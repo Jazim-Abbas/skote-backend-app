2e508ec47de9de5d537c29de7d3e0b05360b7a44
RanaJazim <stackdeveloper87@gmail.com>
2021-04-09 00:36:25 -0700

    validate form requset for storing record of target market module ...

--- controllers/business/target_market.js
+++ controllers/business/target_market.js
@@ -1,3 +1,5 @@
+const schemaValidate = require("../../utils/validations/validate");
+const validation = require("../../utils/validations/target_market");
 const targetMarketService = require("../../services/business/target_market");
 
 async function fetchSingle(req, res) {
@@ -6,7 +8,8 @@ async function fetchSingle(req, res) {
 }
 
 async function store(req, res) {
-  const targetMarket = await targetMarketService.store(req.body, req.user._id);
+  const fields = await schemaValidate(validation.marketSchema, req.body);
+  const targetMarket = await targetMarketService.store(fields, req.user._id);
   res.send({ targetMarket });
 }
 
