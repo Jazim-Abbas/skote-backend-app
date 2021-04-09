70834ab79b4be2c52569b8c1595d1f245bd3e5a9
RanaJazim <stackdeveloper87@gmail.com>
2021-04-09 02:48:59 -0700

    create record of customer and save to database ..

--- controllers/business/customer.js
+++ controllers/business/customer.js
@@ -1,5 +1,8 @@
+const customerService = require("../../services/business/customer");
+
 async function store(req, res) {
-  res.send("store");
+  const customer = await customerService.store(req.body, req.user._id);
+  res.send({ customer });
 }
 
 module.exports = { store };
---
+++ services/business/customer.js
@@ -0,0 +1,17 @@
+const Customer = require("../../db/customer");
+const Exceptions = require("../../utils/custom_exceptions");
+
+async function store(detail, user_id) {
+  const record = new Customer({
+    user: user_id,
+    ...detail,
+  });
+
+  try {
+    return await record.save();
+  } catch (err) {
+    throw new Exceptions.BadRequset("You already created the record");
+  }
+}
+
+module.exports = { store };
