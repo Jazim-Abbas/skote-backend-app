92d16d91b2461dd2c4f39005d5905d5976793b13
jazim-abbas <jazimabbas87@gmail.com>
2021-04-19 12:13:59 +0500

    fixing the bug: getting all users without admin ...

--- services/user.js
+++ services/user.js
@@ -1,7 +1,9 @@
 const User = require("../db/user");
 
 async function getAllUsersWithoutAdmin() {
-  return await User.find({ is_admin: false }).select("-password");
+  return await User.find({
+    $or: [{ is_admin: false }, { is_admin: null }],
+  }).select("-password");
 }
 
 module.exports = {
